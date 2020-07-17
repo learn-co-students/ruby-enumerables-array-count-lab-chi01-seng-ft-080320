0585e2cabde19770c72bf444118956a4d00e0747
John Easter <john@owltestprep.com>
2020-07-17 14:10:55 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,15 @@
-def count_strings(array)
+def count_strings(array = ["", "hello", "world", 4, 1, 5, [], {}])
+  array.count { |element| element.class == String }
+end
+  
+  
+  
   # Return the total number of strings in the provided array using the count enumerable
-end
 
-def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+
+def count_empty_strings(array = ["", "hello", "world", 4, 1, 5, [], {}])
+  array.count { |element| element == "" }
 end
+  
+  
+  # Return the total number of EMPTY strings in the provided array using the count enumerable
